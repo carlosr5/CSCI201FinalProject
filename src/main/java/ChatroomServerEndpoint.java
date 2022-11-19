9e88353a7a73ee9cfc2245d60fa8941ab1328316
Carlos Ramirez <carlosr5@usc.edu>
2022-11-18 21:52:01 -0800

    Adding web socket and chat changes

--- src/main/java/ChatroomServerEndpoint.java
+++ src/main/java/ChatroomServerEndpoint.java
@@ -1,8 +1,44 @@
+import java.io.IOException;
 import java.util.Collections;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
 
+import javax.websocket.OnClose;
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
 import javax.websocket.server.ServerEndpoint;
 
 @ServerEndpoint("/chatroomServerEndpoint")
 public class ChatroomServerEndpoint {
-
+	static Set<Session> users = Collections.synchronizedSet(new HashSet<Session>());
+	
+	@OnOpen
+	public void handleOpen(Session userSession) {
+		users.add(userSession);
+	}
+	
+	@OnMessage
+	public void handleMessage(String message, Session userSession) throws IOException {
+//		If the user exists in the set of current users, then we send the message to all users connected
+//		Otherwise, we add the user to the set and then say that they're connected
+		String username = (String) userSession.getUserProperties().get("username");
+		
+		if(username == null) {
+			userSession.getUserProperties().put("username", message);
+			userSession.getBasicRemote().sendText("System: You are now connected as " + message);
+		}
+		else {
+			Iterator<Session> iter = users.iterator();
+			while (iter.hasNext()) {
+				iter.next().getBasicRemote().sendText(username + ": " + message + '\n');
+			}
+		}
+	}
+	
+	@OnClose
+	public void handleClose(Session userSession) {
+		users.remove(userSession);
+	}
 }
--- src/main/webapp/js/chatClient.js
+++ src/main/webapp/js/chatClient.js
@@ -4,12 +4,12 @@ let socket = new WebSocket("ws://localhost:8080/CSCI201_Final_Project/chatroomSe
 
 // Listening for a message to be sent from the client
 socket.addEventListener('message', (message) => {
-    let JSONData = JSON.parse(message.data);
+    let userMessage = message.data;
 
     // Output the JSON to the console to see the structure that it has
-    if(JSONData.message != null) {
+    if (userMessage != null) {
         // Parse the message data to get the user and the message
-
+        messageTextBox.value += userMessage + '\n';
         // Output each appropriately depending on the HTML structure
     }
 });
