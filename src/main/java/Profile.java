955167c31068006d485e264222be25dc07d61381
George Yang <georgeya@usc.edu>
2022-11-18 22:32:40 -0800

    registration/login page

---
+++ src/main/java/Profile.java
@@ -0,0 +1,37 @@
+import java.io.IOException;  
+import java.io.PrintWriter;  
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;  
+import javax.servlet.http.HttpServletRequest;  
+import javax.servlet.http.HttpServletResponse;  
+import javax.servlet.http.HttpSession;  
+@WebServlet("/profile")
+public class Profile extends HttpServlet {  
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)  
+                      throws ServletException, IOException {
+    	//System.out.println("got request?");
+    	//response.sendRedirect("account.html");
+        response.setContentType("text/html");  
+        PrintWriter out=response.getWriter();  
+        request.getRequestDispatcher("account.html").include(request, response);  
+          
+        HttpSession session=request.getSession(false);  
+        if(session!=null){  
+        String name=(String)session.getAttribute("username");  
+          
+        out.print("Hello, "+name+" Welcome to Profile");
+        out.print("<br/><a href=\"logout\">Logout</a>");
+        }  
+        else{  
+            request.getRequestDispatcher("home.html").include(request, response);  
+            out.print("<h1>Please login first!</h1>");  
+        }  
+        out.close();  
+    }  
+}  
