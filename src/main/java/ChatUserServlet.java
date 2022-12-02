d10f5c40d38a6ab47cd19d0180db9c09603f0528
George Yang <georgeya@usc.edu>
2022-12-01 20:15:33 -0800

    updated headers, fixed chat connection

--- src/main/java/ChatUserServlet.java
+++ src/main/java/ChatUserServlet.java
@@ -6,7 +6,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;  
 import javax.servlet.http.HttpServletResponse;  
 import javax.servlet.http.HttpSession;  
-@WebServlet("/index")
+@WebServlet("/chatRoom")
 public class ChatUserServlet extends HttpServlet {  
     /**
 	 * 
